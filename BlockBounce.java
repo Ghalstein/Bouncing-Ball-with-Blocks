da5f7e32a54831b81508f0ea0bfd9b7eba50b38a
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-08 13:24:45 -0400

    linted the program

--- BlockBounce.java
+++ BlockBounce.java
@@ -29,65 +29,67 @@ public class BlockBounce {
 	private static boolean quit = true;
  
     private static void setup() {
-        // prepare the canvas                                                                                        
-        // set the scale of the coordinate system                                                                    
-        StdDraw.setXscale(-xScale-border, xScale+border);
-        StdDraw.setYscale(-yScale-border, yScale+border);
-        StdDraw.enableDoubleBuffering();   // smooths the animation
-        // initial values of the animnation                                                                          
-        rx = 0.480;             // ball position                                                                     
-        ry = 0.860;
-        vx = 0.015 * xScale;    // velocity                                                                          
-        vy = 0.023 * yScale;
-        radius = xScale / 20;   // radius                                                                            
+      // prepare the canvas                                                                                        
+      // set the scale of the coordinate system                                                                    
+      StdDraw.setXscale(-xScale-border, xScale+border);
+      StdDraw.setYscale(-yScale-border, yScale+border);
+      StdDraw.enableDoubleBuffering();   // smooths the animation
+      // initial values of the animnation                                                                          
+      rx = 0.480;             // ball position                                                                     
+      ry = 0.860;
+      vx = 0.015 * xScale;    // velocity                                                                          
+      vy = 0.023 * yScale;
+      radius = xScale / 20;   // radius                                                                            
     }
+
     private static void updateBallPosition() {
         // bounce off wall according to law of elastic collision                                                     
-	 if ((Math.abs(rx + vx) > xScale - radius)) vx = -vx;
-        if (Math.abs(ry + vy) > yScale - radius) vy = -vy;
-		// detect block 
-		detectBlock(); // detection method is called
-        // update position                                                                                           
-        rx = rx + vx;
-        ry = ry + vy;
-    }
-	private static void detectBlock(){ // Detects if a ball is near a box and manipulates the direction of the ball upon detection
-		for(int i = 0; i < trueBlocksX.size(); i++){ 
-			if((((trueBlocksX.get(i) + 1.5*radius) > (rx + vx)) && (trueBlocksX.get(i) < (rx + vx))) && // detects right side of box
-			((trueBlocksY.get(i) + 1.5*radius) > (ry + vy) && (trueBlocksY.get(i) - 1.5*radius) < (ry + vy))){
-				vx = -vx;
-				if(flag){
-					disappear(i);
-					return;
+		  if ((Math.abs(rx + vx) > xScale - radius)) vx = -vx;
+	      if (Math.abs(ry + vy) > yScale - radius) vy = -vy;
+				// detect block 
+				detectBlock(); // detection method is called
+	        // update position                                                                                           
+	        rx = rx + vx;
+	        ry = ry + vy;
+	  }
+
+		private static void detectBlock(){ // Detects if a ball is near a box and manipulates the direction of the ball upon detection
+			for(int i = 0; i < trueBlocksX.size(); i++){ 
+				if((((trueBlocksX.get(i) + 1.5*radius) > (rx + vx)) && (trueBlocksX.get(i) < (rx + vx))) && // detects right side of box
+				((trueBlocksY.get(i) + 1.5*radius) > (ry + vy) && (trueBlocksY.get(i) - 1.5*radius) < (ry + vy))){
+					vx = -vx;
+					if(flag){
+						disappear(i);
+						return;
+					}
 				}
-			}
-			if((((trueBlocksX.get(i) - 1.5*radius) < (rx + vx)) && (trueBlocksX.get(i) > (rx + vx))) && // detects left side of box
-			((trueBlocksY.get(i) + 1.5*radius) > (ry + vy) && (trueBlocksY.get(i) - 1.5*radius) < (ry + vy))){
-				vx = -vx;
-				if(flag){
-					disappear(i);
-					return;
+				if((((trueBlocksX.get(i) - 1.5*radius) < (rx + vx)) && (trueBlocksX.get(i) > (rx + vx))) && // detects left side of box
+				((trueBlocksY.get(i) + 1.5*radius) > (ry + vy) && (trueBlocksY.get(i) - 1.5*radius) < (ry + vy))){
+					vx = -vx;
+					if(flag){
+						disappear(i);
+						return;
+					}
 				}
-			}
-			if((((trueBlocksY.get(i) + 1.5*radius) > (ry + vy)) && (trueBlocksY.get(i) < (ry + vy))) && // detects top of box
-			((trueBlocksX.get(i) + 1.5*radius) > (rx + vx) && (trueBlocksX.get(i) - 1.5*radius) < (rx + vx))){
-				vy = -vy;
-				if(flag){
-					disappear(i);
-					return;
+				if((((trueBlocksY.get(i) + 1.5*radius) > (ry + vy)) && (trueBlocksY.get(i) < (ry + vy))) && // detects top of box
+				((trueBlocksX.get(i) + 1.5*radius) > (rx + vx) && (trueBlocksX.get(i) - 1.5*radius) < (rx + vx))){
+					vy = -vy;
+					if(flag){
+						disappear(i);
+						return;
+					}
 				}
-			}
-			if((((trueBlocksY.get(i) - 1.5*radius) < (ry + vy)) && (trueBlocksY.get(i) > (ry + vy))) && // detects bottom of box
-			((trueBlocksX.get(i) + 1.5*radius) > (rx + vx) && (trueBlocksX.get(i) - 1.5*radius) < (rx + vx))){
-				vy = -vy;
-				if(flag){
-					disappear(i);
-					return;
+				if((((trueBlocksY.get(i) - 1.5*radius) < (ry + vy)) && (trueBlocksY.get(i) > (ry + vy))) && // detects bottom of box
+				((trueBlocksX.get(i) + 1.5*radius) > (rx + vx) && (trueBlocksX.get(i) - 1.5*radius) < (rx + vx))){
+					vy = -vy;
+					if(flag){
+						disappear(i);
+						return;
+					}
 				}
-			}
-			
-		}	
-	}
+				
+			}	
+		}
 
 	private static void disappear(int i){ // When flagged the program causes a box to disappear upon detection
 		if(flag){
@@ -98,6 +100,7 @@ public class BlockBounce {
 			trueBlocksY.clear();
 		}
 	}
+
 	private static void generateBlocks(int N){ 
 		// createes boxes at a random location on the grid
 		for(int i = 0; i < N; i++){
@@ -107,8 +110,9 @@ public class BlockBounce {
 				i--;
 				continue;
 			}else grid[tempX][tempY] = true;
-			}
 		}
+	}
+
 	private static void grid(){
 		//generate a grid on top of the canvas
 		int count = 0;
@@ -131,27 +135,30 @@ public class BlockBounce {
 				}
 			}
 	}
-    private static void repaint() {
+
+  private static void repaint() {
 	// clear the background                                                                                      
-        StdDraw.clear();
-        StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
-        StdDraw.filledRectangle(0, 0, xScale, yScale);
-        // draw ball on the screen
-		grid();
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(rx, ry, radius);
-        StdDraw.show();
-    }
-    public static void main(String[] args) {
-        if (args.length < 1 || args.length > 2) {
-            System.out.println("Usage: java BlockBounce N");
+      StdDraw.clear();
+      StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+      StdDraw.filledRectangle(0, 0, xScale, yScale);
+      // draw ball on the screen
+	grid();
+      StdDraw.setPenColor(StdDraw.BLACK);
+      StdDraw.filledCircle(rx, ry, radius);
+      StdDraw.show();
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 1 || args.length > 2) {
+      System.out.println("Usage: java BlockBounce N");
 			System.out.println("Usage: java BlockBounce N -d");
-            System.exit(0);
-        }
+      System.exit(0);
+    }
 		int N = 0;
 		try{
 			N = Integer.parseInt(args[0]);
-		} catch(NumberFormatException e){
+		} 
+		catch(NumberFormatException e){
 				System.out.print("Sorry, you did not enter a number.");
 				System.exit(1);
 		}
@@ -159,25 +166,27 @@ public class BlockBounce {
 			System.out.println("Sorry, you have entered a number less than 1.");
 			System.exit(1);
 		}
-		if(args.length == 2)
+		if(args.length == 2) {
 			if(args[1].equals("-d")){
 				flag = true;
-			}else{
+			}
+			else{
 				System.out.print("Sorry, you did not enter flag, -d, for the second argument.");
 				System.exit(1);
 			}
+		}
 		// canvas and initial values                                                                                 
-        setup();
+    setup();
 		generateBlocks(N);
-        // main animation loop                                                                                       
-        while (quit)  {
-            updateBallPosition();
-            repaint();
-            StdDraw.pause(20);
+    // main animation loop                                                                                       
+    while (quit)  {
+      updateBallPosition();
+      repaint();
+      StdDraw.pause(20);
 			if(!quit){ // Flag to quit the program
 				StdDraw.pause(3000);
 				System.exit(1);
 			}
-        }
     }
+	}
 }
